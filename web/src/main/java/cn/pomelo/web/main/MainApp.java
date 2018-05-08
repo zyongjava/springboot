c2a3b6d5680b4129896347842dd5f2245ad21366
zhengyong <yong.zheng@tongdun.cn>
2018-05-08 16:54:03 +0800

    fix

---
+++ web/src/main/java/cn/pomelo/web/main/MainApp.java
@@ -0,0 +1,25 @@
+package cn.pomelo.web.main;
+
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@EnableAutoConfiguration
+@SpringBootApplication
+@EnableWebMvc
+@ImportResource(locations = {"/spring/service-root.xml"})
+public class MainApp extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(MainApp.class);
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(MainApp.class, args);
+    }
+
+}
